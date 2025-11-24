2e1add87c5616b0fe7c14b34686008bf0b7acc8b
Admin <admin@MAC926-10.local>
2025-11-24 09:30:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/Second': (context) => const SecondPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.red[900]),
+
+                child: Column(
+                  children: [
+                    Text(
+                      "ข้อมูลส่วนตัว",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+
+                      child: ClipOval(
+                        child: Image.network(
+                          "https://thethaiger.com/th/wp-content/uploads/2022/10/%E0%B8%9E%E0%B8%A3%E0%B8%B5%E0%B9%88%E0%B8%84%E0%B8%B2%E0%B8%8B%E0%B8%B6%E0%B8%A2%E0%B8%B0-%E0%B8%AD%E0%B8%AD%E0%B8%81%E0%B8%81%E0%B8%B3%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A2-1.jpg",
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text("Katsuya"),
+                    SizedBox(height: 5),
+                    Text("Prekatsuya@e-tech.ac.th"),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("ข้อมูลส่วนตัว"),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.green[100],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Icon(Icons.phone, color: Colors.green),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [Text("เบอร์โทรศัพท์"), Text("0649140879")],
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.pink[100],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Icon(Icons.cake, color: Colors.pinkAccent),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [Text("วันเกิด"), Text("14 กันยายน 2548")],
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.orange[100],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Icon(Icons.pin_drop, color: Colors.orangeAccent),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [Text("ที่อยู่"), Text("ชลบุรี")],
+                        ),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.purple[100],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Icon(Icons.school, color: Colors.purpleAccent),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: [Text("การศึกษา"), Text("วิทยาลัยเทคโนโลยีภาคตะวันออก(อีเทค)")],
+                        ),
+                      ],
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () => Navigator.pushNamed(context, '/Second'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 1,
+                      ),
+                      child: Text(" Go to Second"),
+                    ),
+                  ],
+
+                ),
+              ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Second Page")));
+  }
+}
